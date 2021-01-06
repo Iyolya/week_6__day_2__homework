ffcad780dde8cdd3debb741434787801ec8a02ec
Lyolya <almasilaura@outlook.com>
2021-01-06 23:20:15 +0000

    SET UP 'park' model

---
+++ models/park.js
@@ -0,0 +1,13 @@
+const Park = function (name, ticket_price) {
+    this.name = name;
+    this.ticket_price = ticket_price;
+    this.dinosaurs = [];
+  }
+  
+  module.exports = Dinosaur;
+  
+
+
+
+
+
